2c6d674ccb761b20b350c664025e43ca8c418a12
Steewar Martinez <steewarmartinez@gmail.com>
2025-08-13 13:43:18 -0300

    "Datos de contraseña ingresados iguales"

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,18 @@
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+  document.getElementById("alert-success").classList.add("show");
 }
 
 function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+  document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click", function () {
+  let pass1 = document.getElementById("password1").value;
+  let pass2 = document.getElementById("password2").value;
+
+  if (pass1 === pass2 && pass1.trim() !== "") {
+    showAlertSuccess();
+  } else {
+    showAlertError();
+  }
+});
