057884872edb644eb61ee2bdbb0d7db45d34e239
BrianON3 <64492169+BrianON3@users.noreply.github.com>
2025-08-13 13:43:22 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,21 @@
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+  document.getElementById("alert-success").classList.add("show");
 }
 
 function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+  document.getElementById("alert-danger").classList.add("show");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const regBtn = document.getElementById("regBtn");
+
+  regBtn.addEventListener("click", function() {
+    const termsAccepted = document.getElementById("terminos").checked;
+
+    if (termsAccepted) {
+      showAlertSuccess(); // Checkbox marcado
+    } else {
+      showAlertError();   // Checkbox NO marcado
+    }
+  });
+});
