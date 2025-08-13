e7c583e44004fad0bf98f4e751a62b0448ca6980
martin-r1307 <martinruppel.mr@gmail.com>
2025-08-13 13:31:30 -0300

    Contraseñas no son menores a 6

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,21 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click", function () {
+    let password1 = document.getElementById("password1").value.trim();
+
+if (password1.length < 6) {
+         showAlertError();
+        return;
+}
+});
+
+document.getElementById("regBtn").addEventListener("click", function () {
+    let password2 = document.getElementById("password2").value.trim();
+
+if (password2.length < 6) {
+         showAlertError();
+        return;
+}
+});
